10576ab9537c0bb0bbf5517b55b88be7f16aa75a
aleezk <droidealejandrok@gmail.com>
2026-02-02 18:26:13 -0400

    Tercer commit

--- controlador/controlador.java
+++ controlador/controlador.java
@@ -4,30 +4,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import vista.vista;
-//import modelo.modelo;
+import modelo.modelo;
 
-public class controlador implements ActionListener {
-    
+public class controlador implements ActionListener{
     private vista vista;
-    //private modelo modelo;
+    private modelo modelo;
 
     public controlador() {
         this.vista = new vista();
-        //this.modelo = new modelo();
+        this.modelo = new modelo();
 
-        //this.vista.setControlador(this);
+        this.vista.setControlador(this);
         this.vista.setVisible(true);
     }
 
+    @Override
     public void actionPerformed(ActionEvent a) {
         String z = a.getActionCommand();
 
         switch (z) {
             case "login":
-                //login();
+                login();
                 break;
             case "registrar":
-                //Registro();
+                Registro();
                 break;
             case "salir":
                 System.exit(0);
@@ -35,30 +35,29 @@ public class controlador implements ActionListener {
         }
     }
 
-    /*private void login() {
-       
+    private void login() {
         String usuario = vista.getUsuario();
         String cntr = vista.getContra();
 
-        boolean v = modelo.verificar(usuario, cntr);
+        boolean v = this.modelo.verificar(usuario, cntr);
 
         if (v) {
             JOptionPane.showMessageDialog(vista, "hola " + usuario);
         } else {
             JOptionPane.showMessageDialog(vista, "error: usuario o contraseña incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
         }
-    }*/
+    }
 
-    /*private void Registro() {
-        String uusario = vista.getUsuario();
+    private void Registro() {
+        String usuario = vista.getUsuario();
         String cntr = vista.getContra();
 
         if(usuario.isEmpty() || cntr.isEmpty()){
-        JOptionPane.showMessageDialog(vista, "se tiene que llenar todos los datos para registrarse");
-        return;
+            JOptionPane.showMessageDialog(vista, "se tiene que llenar todos los datos para registrarse");
+            return;
         }
         
-        boolean guardado = modelo.registrarUsuario(usuario, cntr);
+        boolean guardado = this.modelo.registrarUsuario(usuario, cntr);
 
         if (guardado) {
             JOptionPane.showMessageDialog(vista, "usuario registrado");
@@ -66,5 +65,5 @@ public class controlador implements ActionListener {
         } else {
             JOptionPane.showMessageDialog(vista, "error al registrarse", "Error", JOptionPane.WARNING_MESSAGE);
         }
-    }*/
+    }
 }
