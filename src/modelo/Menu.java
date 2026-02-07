f9749dadfbfce9acb809a193ac8396911dabe182
luismmonteroz-cell <luis.mmonteroz@gmail.com>
2026-02-07 12:09:05 -0400

    limpieza

--- src/modelo/Menu.java
+++ src/modelo/Menu.java
@@ -4,7 +4,7 @@ public class Menu {
     private String fecha;
     private String tipoPlato;
     private int cantidadRaciones;
-    private double costoUnitario; // Nuevo campo para el CCB
+    private double costoUnitario; 
 
     public Menu(String fecha, String tipoPlato, int cantidadRaciones, double costoUnitario) {
         this.fecha = fecha;
@@ -13,20 +13,17 @@ public class Menu {
         this.costoUnitario = costoUnitario;
     }
 
-    // Getters
     public String getFecha() { return fecha; }
     public String getTipoPlato() { return tipoPlato; }
     public int getCantidadRaciones() { return cantidadRaciones; }
     public double getCostoUnitario() { return costoUnitario; }
 
-    // Para guardar en TXT: Fecha#Plato#Raciones#Costo
     public String toTXT() {
         return fecha + "#" + tipoPlato + "#" + cantidadRaciones + "#" + costoUnitario;
     }
     
     @Override
     public String toString() {
-        // Redondeamos el precio a 2 decimales para que se vea bien
         return "Fecha: " + fecha + " | Plato: " + tipoPlato + " | Costo CCB: " + String.format("%.2f", costoUnitario) + " Bs.";
     }
 }
