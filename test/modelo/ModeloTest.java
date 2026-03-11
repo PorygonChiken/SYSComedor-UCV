938ef4b66934784d8d9aa6eb3e0998f42ab845f8
aleezk <droidealejandrok@gmail.com>
2026-03-11 17:52:32 -0400

    Prueba unitaria para SaldoPana

--- test/modelo/ModeloTest.java
+++ test/modelo/ModeloTest.java
@@ -48,4 +48,42 @@ public class ModeloTest {
 
         assertEquals(saldoEsperado, saldoReal, "Un usuario que no existe debe tener saldo 0");
     }
+
+    @Test
+    public void testTransferirSaldoPanaExitoso() {
+       
+        modelo miModelo = new modelo();
+        
+        String origen = "Pana1";
+        String cedulaOrigen = "11111111";
+        miModelo.registrarUsuario(origen, cedulaOrigen, "clave123");
+        miModelo.recargarSaldo(origen, 500.0); 
+
+        String destino = "PanaRecibe";
+        String cedulaDestino = "22222222";
+        miModelo.registrarUsuario(destino, cedulaDestino, "clave123"); 
+
+        double montoATransferir = 150.0;
+
+        boolean transferenciaExitosa = miModelo.transferirSaldoPana(origen, cedulaDestino, montoATransferir);
+
+        assertEquals(true, transferenciaExitosa, "La transferencia debería retornar true al completarse con saldo suficiente");
+    }
+
+    @Test
+    public void testTransferirSaldoPanaSinDinero() {
+       
+        modelo miModelo = new modelo();
+        
+        String origen = "Pana2";
+        String cedulaOrigen = "33333333";
+        miModelo.registrarUsuario(origen, cedulaOrigen, "clave123");
+       
+        String cedulaDestino = "22222222"; 
+        double montoATransferir = 1000.0; 
+
+        boolean transferenciaExitosa = miModelo.transferirSaldoPana(origen, cedulaDestino, montoATransferir);
+
+        assertEquals(false, transferenciaExitosa, "La transferencia debe fallar si el origen no tiene saldo suficiente");
+    }
 }
